c0ef095d8f13f9efeceb0a173d52752ce5479c99
Karthikk508 <karthik.kandhasamy508@gmail.com>
2025-01-13 21:31:19 +0530

    Added validations

--- src/main/java/com/karthik/rest/webservices/restful_web_services/user/User.java
+++ src/main/java/com/karthik/rest/webservices/restful_web_services/user/User.java
@@ -2,11 +2,17 @@ package com.karthik.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 	
 	
 	private Integer id;
+	
+	@Size(min = 2 , message = "Name should not be empty")
 	private String name;
+	@Past(message = "Date of birth should not be in the future")
 	private LocalDate birthDate;
 	
 	
