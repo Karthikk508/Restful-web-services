064fde478050b8b0a88583242c6d9099cc7814fa
Karthikk508 <karthik.kandhasamy508@gmail.com>
2025-01-15 21:06:22 +0530

    Internationalization

--- src/main/java/com/karthik/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/karthik/rest/webservices/restful_web_services/user/UserResource.java
@@ -3,7 +3,10 @@ package com.karthik.rest.webservices.restful_web_services.user;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +21,13 @@ import jakarta.validation.Valid;
 @RestController
 public class UserResource {
 	
+	
+	private MessageSource messageSource;
 	private UserDaoService service;
 	
-	public UserResource(UserDaoService service) {
+	public UserResource(UserDaoService service,MessageSource messageSource) {
 		this.service = service;
+		this.messageSource = messageSource;
 	}
 	
 	
@@ -60,6 +66,18 @@ public class UserResource {
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		service.deleteById(id);
+	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		
+		Locale locale = LocaleContextHolder.getLocale();		
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+		
+		
+		//return "Hello world v2";
+				
+				
 	}
 	
 
